8fcd682745de88a2854ddf53ec3358de5d0f1a6c
vitekkuz <vitekkuz@mail.ru>
2019-09-26 17:44:21 +0300

    Добавлена модель Model

--- service/admin.py
+++ service/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import category
+from .models import Category, Model
 
 # Register your models here.
-admin.site.register(category)
+admin.site.register(Category)
+admin.site.register(Model)
+
 
--- service/models.py
+++ service/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here
-class category(models.Model):
+class Category(models.Model):
     '''' Категория услуг '''
     name = models.CharField('Название категории', max_length=200)
     slug = models.SlugField('URL', max_length=120)
@@ -20,3 +20,29 @@ class category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+
+
+class Model(models.Model):
+    '''' Модель из категории '''
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True,
+                                 verbose_name='Выбор категории')
+    name = models.CharField('Название услуги', max_length=120)
+    slug = models.SlugField('URL', max_length=120, unique=True)
+    text = models.TextField('Текст', default='')
+    header = models.CharField('Заголовок', max_length=240, blank=True, null=True)
+    sub_header = models.CharField('Подзаголовок', max_length=240, blank=True, null=True)
+    images = models.ImageField('Картинка техники', upload_to='images/', blank=True, null=True)
+    active = models.BooleanField('Опубликовать', default=True)
+    banner = models.ImageField('Баннер', upload_to='images/', blank=True, null=True)
+    title = models.CharField('Title', max_length=120)
+    descripton = models.CharField('Description', max_length=250)
+    keywords = models.CharField('Keywords', max_length=250)
+    sort = models.PositiveIntegerField('Порядок', default=0, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    # прописываем, как будет отоборажаться название модели в админке
+    class Meta:
+        verbose_name = 'Модель'
+        verbose_name_plural = 'Модели'
